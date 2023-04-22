1fe945f447b020e3a18f2098687fe1b5c25db320
Diego Vásquez <dvasquezar@gmail.com>
2023-04-21 22:26:53 -0600

    Add fields to users table

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->enum('type', ['admin', 'doctor', 'patient']);
             $table->string('email')->unique();
+            $table->string('dui')->unique();
+            $table->enum('genre', ['m', 'f']);
+            $table->date('birthday');
+            $table->string('phone');
+            $table->text('address_1');
+            $table->text('address_2');
+            $table->string('trusted_contact');
+            $table->string('trusted_contact_phone');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
